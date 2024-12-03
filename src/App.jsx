fae39743f00ea94f647acca96a467645f20a8ec1
behhruz <bsatimbayev05@gmail.com>
2024-12-03 16:45:08 +0500

    start project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./Components/Navbar"
+import Portners from "./Components/Portners"
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+
+
+      <main>
+        <Portners />
+      </main>
+    </>
+
+  )
+}
+
+export default App

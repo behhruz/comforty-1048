91880b4c1e23a75360e8b590730d3782d04ce065
Benjamin <RichMan@MacBook-Pro-RichMan.local>
2024-12-07 16:55:20 +0500

    done

---
+++ src/Dashboard.jsx
@@ -0,0 +1,32 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./Components/Navbar";
+// import Portners from "./Components/Portners";
+// import Ot from "./Components/ot";
+// import Categories from "./Components/Category";
+// import Xm from "./Components/Cards";
+
+// function Dashboard() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={
+//             <>
+//               <Navbar />
+//               <main>
+//                 <Portners />
+//                 <Ot />
+//                 <Categories />
+//                 <Xm />
+//               </main>
+//             </>
+//           }
+//         />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default Dashboard;

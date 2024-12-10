5ac2d1512309c28c6012e1534a423af1fa972645
LolaAmirova <benjamenalex913@gmail.com>
2024-12-10 16:53:53 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./Dashboard/Login";
 import Register from "./Dashboard/Register";
 import AdminPanel from "./Dashboard/AdminPanel";
 import Navbar from "./Components/Navbar";
-import Portners from "./Components/Portners";
-import Ot from "./Components/ot";
-import Categories from "./Components/Category";
-import Xm from "./Components/Cards";
-import Carusel from "./Components/Carousel";
+import Home from "./page/Home";
+import About from "./page/About";
+import Shop from "./page/Shop";
+import Product from "./page/Product";
+import Pages from "./page/Pages";
 
 function App() {
   return (
@@ -18,19 +23,49 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/" element={<Navigate to="/login" />} />
-
         <Route
           path="/home"
           element={
             <>
               <Navbar />
-              <Carusel />
-              <main>
-                <Portners />
-                <Ot />
-                <Categories />
-                <Xm />
-              </main>
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/shop"
+          element={
+            <>
+              <Navbar />
+              <Shop />
+            </>
+          }
+       
+        />
+        <Route
+          path="/product"
+          element={
+            <>
+              <Navbar />
+              <Product />
+            </>
+          }
+        />
+         <Route
+          path="/pages"
+          element={
+            <>
+              <Navbar />
+              <Pages />
+            </>
+          }
+      />
+        <Route
+          path="/about"
+          element={
+            <>
+              <Navbar />
+              <About />
             </>
           }
         />
---
+++ src/page/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Portners from '../Components/Portners'
+
+const Home = () => {
+  return (
+    <div>
+      <p></p>
+      <Portners/>
+    </div>
+  )
+}
+
+export default Home
